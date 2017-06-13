4ee9861ce70b00c00990e53523b9afa82273b41c
Bonello, Josh <josh.bonello@egov.com>
2017-06-13 16:26:46 -0500

    User Security implemented, server up but buggy

--- collection/views.py
+++ collection/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from collection.models import Thing
 from collection.forms import ThingForm
+from django.template.defaultfilters import slugify
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 
@@ -14,11 +17,14 @@ def thing_detail(request, slug):
 
                 # and pass to the template
         return render(request, 'things/thing_detail.html', { 'thing': thing, })
-
+@login_required
 def edit_thing(request, slug):
     #grab the object
     thing = Thing.objects.get(slug=slug)
     # set the form we're using
+    #make sure the logged in user is the owner of the thing
+    if thing.user != request.user:
+        raise Http404
     form_class = ThingForm
 
     # if we're coming to this view from a submitted form
@@ -34,3 +40,25 @@ def edit_thing(request, slug):
         form = form_class(instance=thing)
     # and render the template
     return render(request, 'things/edit_thing.html',{ 'thing' : thing, 'form' : form, })
+
+def create_thing(request):
+    form_class = ThingForm
+    if request.method == 'POST':
+        #grab the data from the submitted form and apply to the form
+        form = form_class(request.POST)
+        if form.is_valid():
+            #create an instance but don't save yet
+            thing = form.save(commit=False)
+            #set the additional details
+            thing.user = request.user
+            thing.slug = slugify(thing.name)
+            #save the object
+            thing.save()
+            #redirect to newly created thing
+            return redirect('thing_detail', slug=thing.slug)
+        #otherwise just create the form
+    else:
+        form = form_class()
+    return render(request, 'things/create_thing.html', { 'form' : form })
+
+
